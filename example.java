48b4a50cb69037831bfd2467f92c04ae893a9f5d
padmapriyaravi08 <padmapriyaravi08@gmail.com>
2025-12-09 17:26:12 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,13 @@
+public class example{
+  public static void main(String args[]){
+    System.out.println("welcome to learn java");
+    int a=6;
+    int b=66;
+    int c;
+    c=a+b;
+    System.out.println("the addition of a and b is "+c);
+  }
+}
+  
+  
+    
