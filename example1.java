7ebda49316e3cb413cc37509d574c635d8f43559
padmapriyaravi08 <padmapriyaravi08@gmail.com>
2025-12-09 17:52:55 +0530

    Add files via upload

---
+++ example1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class example1{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    String name=sc.next();
+    System.out.println("welcome to learn java "+name);
+  }
+}
