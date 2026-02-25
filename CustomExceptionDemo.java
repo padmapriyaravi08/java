309350560993421549addfde5aa9c546dc72e543
padmapriyaravi08 <padmapriyaravi08@gmail.com>
2026-02-25 18:03:54 +0530

    Add files via upload

---
+++ CustomExceptionDemo.java
@@ -0,0 +1,25 @@
+public class CustomExceptionDemo {
+
+    public static void main(String[] args) {
+        try {
+            checkAge(16);
+        } catch (InvalidAgeException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+    }
+
+    static void checkAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above");
+        } else {
+            System.out.println("Eligible to vote");
+        }
+    }
+}
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+
