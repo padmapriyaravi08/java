73107293831d59d636ddce2db6e35887c006907d
padmapriyaravi08 <padmapriyaravi08@gmail.com>
2026-02-27 17:19:41 +0530

    Add files via upload

---
+++ stud details.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class stud_details{
+  int rollno;
+  System.out.println("enter your rollno:");
+  String name;
+  System.out.println("enter your name:");
+  int age;
+  System.out.println("enter your age:");
+  int mark1;
+  System.out.println("enter your mark1:");
+  int mark2;
+  System.out.println("enter your mark2:");
+  int mark3;
+  System.out.println("enter your mark3:");
+}
+  
+  
+
+  
+  
+  
