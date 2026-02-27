4514966f7d4e4f7fccb3c91a7b523f8a1f4a19b8
padmapriyaravi08 <padmapriyaravi08@gmail.com>
2026-02-27 17:22:15 +0530

    Add files via upload

---
+++ TryCatchDemo.java
@@ -0,0 +1,11 @@
+class TryCatchDemo{
+  public static void main(String[] args){
+    try{
+      int a=10/0;
+    }
+    catch(ArithmeticException e){
+      System.out.println("cannot divide by zero");
+    }
+  }
+}
+  
