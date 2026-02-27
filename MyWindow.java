e4d26aafe7d064694798f18ca44f3be454150ebc
padmapriyaravi08 <padmapriyaravi08@gmail.com>
2026-02-27 18:04:51 +0530

    Add files via upload

---
+++ MyWindow.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class MyWindow {
+  public static void main(String[] args) {
+    JWindow window = new JWindow();
+    window.setSize(600, 500);
+    window.setVisible(true);
+  }
+}
